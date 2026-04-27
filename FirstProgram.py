e90924267804ffcda4efc4c3eebf1062f5960cb9
Dipanshu Bisen <dipanshubisen15@gmail.com>
2026-04-27 10:12:11 +0530

    first project

---
+++ FirstProgram.py
@@ -0,0 +1,19 @@
+print("Hello")
+print("hi","Dipanshu")
+print(23)
+print(5+4)
+
+name = "Dipanshu"
+print(name)
+print("My Name is :", name)
+
+print(type(name))
+
+a = 3
+b = 4
+sum =a + b
+print(sum)
+
+
+myname= input("Enter your name :")
+print ("My name is :", myname)
